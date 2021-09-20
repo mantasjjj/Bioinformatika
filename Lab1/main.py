c37cc0d81c3a10cca68e1a5d4293091ce2ae992c
mantasjjj <mjakaitis18@gmail.com>
2021-09-20 22:12:26 +0300

    added method for dicodon frequency calculation and finished codon frequency calculation method

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,9 +1,12 @@
 import numpy as np
 from Bio import SeqIO
+from Bio.Data.CodonTable import unambiguous_dna_by_name
 
 
 # 1. Pateiktoje sekoje fasta formatu surastu visas start ir stop kodonų poras, tarp kurių nebutu stop kodono (ir tiesioginei sekai ir jos reverse komplementui).
 # Susirandam visas imanomas kombinacijas is sekos
+
+
 def get_triplets(seq):
     frames = []
     frames.append([seq[i:i + 3] for i in range(0, len(seq), 3)])
@@ -63,6 +66,7 @@ def find_farthest_codon_for_each_stop_codon(seq_record):
         i += 1
     return codon_list
 
+
 def find_farthest_starts(triplets):
     starts = []
     for triplet in triplets:
@@ -79,6 +83,56 @@ def find_shortest_codons(codons):
     return shortest_codons
 
 
+# 4. Parasykite funkcijas, kurios ivertintu kodonu ir dikodonu daznius(visi imanomi kodonai/dikodonai ir ju
+# atitinkamas daznis - gali buti nemazai nuliu, jei ju sekoje nerasite).
+def find_codon_frequency(codon_list):
+    standard_table = unambiguous_dna_by_name["Bacterial"]
+    letters = standard_table.nucleotide_alphabet
+    alphabet_list = []
+    for c1 in letters:
+        for c2 in letters:
+            for c3 in letters:
+                codon = c1 + c2 + c3
+                alphabet_list.append(codon)
+    codon_alphabet = dict.fromkeys(alphabet_list, 0.0)
+
+    total = 0
+    for orf in codon_list:
+        codons_in_orf = [orf[i:i + 3] for i in range(0, len(orf), 3)]
+        total += len(codons_in_orf)
+        for codon in codons_in_orf:
+            codon_alphabet[codon] += 1
+    for key in codon_alphabet:
+        codon_alphabet[key] = (codon_alphabet[key] / total) * 100
+    return codon_alphabet
+
+
+def find_dicodon_frequency(codon_list):
+    standard_table = unambiguous_dna_by_name["Bacterial"]
+    letters = standard_table.nucleotide_alphabet
+    alphabet_list = []
+    for c1 in letters:
+        for c2 in letters:
+            for c3 in letters:
+                for c4 in letters:
+                    for c5 in letters:
+                        for c6 in letters:
+                            dicodon = c1 + c2 + c3 + c4 + c5 + c6
+                            alphabet_list.append(dicodon)
+    dicodon_alphabet = dict.fromkeys(alphabet_list, 0.0)
+
+    total = 0
+    for orf in codon_list:
+        dicodons_in_orf = [orf[i:i + 6] for i in range(0, len(orf), 6)]
+        total += len(dicodons_in_orf)
+        for dicodon in dicodons_in_orf:
+            if len(dicodon) == 6:
+                dicodon_alphabet[dicodon] += 1
+    for key in dicodon_alphabet:
+        dicodon_alphabet[key] = (dicodon_alphabet[key] / total) * 100
+    return dicodon_alphabet
+
+
 if __name__ == '__main__':
     record = SeqIO.read("data/bacterial4.fasta", "fasta")
 
@@ -93,9 +147,15 @@ if __name__ == '__main__':
     # 2.
     print("2. Each stop codons farthest start codon")
     farthest_start_codons = find_farthest_starts(triplets)
-    print(farthest_start_codons)
+    # print(farthest_start_codons)
 
     # 3.
     shortest_codons = find_shortest_codons(codons)
-    print("3. Codons that have a length less than 100:")
+    print("3. Codons that have a length less than 100 symbols:")
     # print(shortest_codons)
+
+    print("4. Codon frequency (%):")
+    print(find_codon_frequency(codons))
+
+    print("4. Dicodon frequency (%):")
+    print(find_dicodon_frequency(codons))
