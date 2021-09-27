0253c797dee849b1b9cdfdde2c65065057cd0aa3
mantasjjj <mjakaitis18@gmail.com>
2021-09-27 22:16:13 +0300

    added method for printint the codon and dicodon frequency in the form of a matrix

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,10 +1,14 @@
 import numpy as np
 from Bio import SeqIO
 from Bio.Data.CodonTable import unambiguous_dna_by_name
+import pandas as pd
 
+
+alphabet_list = []
 
 # 1. Pateiktoje sekoje fasta formatu surastu visas start ir stop kodonų poras, tarp kurių nebutu stop kodono (ir tiesioginei sekai ir jos reverse komplementui).
 # Susirandam visas imanomas kombinacijas is sekos
+from scipy.spatial import distance_matrix
 
 
 def get_triplets(seq):
@@ -88,7 +92,6 @@ def find_shortest_codons(codons):
 def find_codon_frequency(codon_list):
     standard_table = unambiguous_dna_by_name["Bacterial"]
     letters = standard_table.nucleotide_alphabet
-    alphabet_list = []
     for c1 in letters:
         for c2 in letters:
             for c3 in letters:
@@ -132,30 +135,65 @@ def find_dicodon_frequency(codon_list):
         dicodon_alphabet[key] = (dicodon_alphabet[key] / total) * 100
     return dicodon_alphabet
 
+# 5. Palyginkite kodonu bei dikodonu daznius tarp visu seku (atstumu matrica - kokia formule naudosite/kaip apskaiciuosite - parasykite ataskaitoje).
+def print_distance_matrix(frequencies):
+    ids = []
+
+    for id in frequencies:
+        ids.append(id)
+
+    data = frequencies[ids[0]]
+
+    print("********", end = ' ')
+    for i in data:
+        print(i, end=' ')
+
+    for i in range(0, 7):
+        print("")
+        print(ids[i], end = ' ')
+        for j in data:
+            print("%.2f" % frequencies[ids[i]][j], end = ' ')
+
 
 if __name__ == '__main__':
-    record = SeqIO.read("data/bacterial4.fasta", "fasta")
+    files = ["bacterial1.fasta", "bacterial2.fasta", "bacterial3.fasta", "bacterial4.fasta", "mamalian1.fasta",
+             "mamalian2.fasta", "mamalian3.fasta", "mamalian4.fasta"]
 
-    triplets = get_triplets(record.seq)
+    codon_frequencies = {}
+    dicodon_frequencies = {}
 
-    # 1.
-    codons = find_codons(triplets)
-    print("1. All codons in a sequence:")
-    codons = np.concatenate(codons)
-    # print(codons)
+    for i in files:
+        record = SeqIO.read("data/" + i, "fasta")
+        triplets = get_triplets(record.seq)
 
-    # 2.
-    print("2. Each stop codons farthest start codon")
-    farthest_start_codons = find_farthest_starts(triplets)
-    # print(farthest_start_codons)
+        print("****************************" + record.id + "***********************************")
+        # 1.
+        codons = find_codons(triplets)
+        # print("1. All codons in a sequence:")
+        codons = np.concatenate(codons)
+        # print(codons)
 
-    # 3.
-    shortest_codons = find_shortest_codons(codons)
-    print("3. Codons that have a length less than 100 symbols:")
-    # print(shortest_codons)
+        # 2.
+        # print("2. Each stop codons farthest start codon")
+        farthest_start_codons = find_farthest_starts(triplets)
+        # print(farthest_start_codons)
 
-    print("4. Codon frequency (%):")
-    print(find_codon_frequency(codons))
+        # 3.
+        shortest_codons = find_shortest_codons(codons)
+        # print("3. Codons that have a length less than 100 symbols:")
+        # print(shortest_codons)
 
-    print("4. Dicodon frequency (%):")
-    print(find_dicodon_frequency(codons))
+        # 4.
+        # print("4. Codon frequency (%):")
+        codon_frequencies[record.id] = find_codon_frequency(codons)
+        # print(find_codon_frequency(codons))
+
+        # print("4. Dicodon frequency (%):")
+        dicodon_frequencies[record.id] = find_dicodon_frequency(codons)
+        # print(find_dicodon_frequency(codons))
+
+    print("Codon frequency matrix:")
+    print_distance_matrix(codon_frequencies)
+
+    print("Dicodon frequency matrix:")
+    print_distance_matrix(dicodon_frequencies)
